2249b3d24abaf186b4f694c88ef8941ffa0ed27f
INNOVATIVEGAMER <prasadpatewar39@gmail.com>
2020-11-27 00:46:07 +0530

    Tweaks in the home page (#14)
    1.Now full responsiveness for home page with any resolution
    2.Now text content can be seen on small screens

--- src/features/Home/HomePage.jsx
+++ src/features/Home/HomePage.jsx
@@ -1,52 +1,64 @@
-import { Container, Grid, Hidden, withStyles } from "@material-ui/core";
-import React, { Component, Fragment } from "react";
+import { Box, Container, Grid, withStyles } from "@material-ui/core";
+import React, { Component } from "react";
 import NavBar from "../NavBar/navbarComponent";
 import AuthenticationPanel from "../Panels/AuthenticationPanel";
 
 const styles = {
-	root: {},
-	Bg: {
-		height: "120vh",
-		background:
-			'url("./assets/HomePage/BGImage.png") no-repeat center center /cover',
-	},
-	darkOverlay: {
-		backgroundColor: "rgba(0,0,0,0.2)",
-		height: "inherit",
-	},
-	authPanel: {
-		margin: "2rem",
-	},
+    root: {},
+    Bg: {
+        height: "100%",
+        background:
+            'url("./assets/HomePage/BGImage.png") no-repeat center center /cover',
+    },
+    darkOverlay: {
+        backgroundColor: "rgba(0,0,0,0.2)",
+        height: "inherit",
+    },
+    authPanel: {
+        margin: "2rem",
+    },
 };
 
 class HomePage extends Component {
-	render() {
-		const { classes } = this.props;
-		return (
-			<Fragment>
-				<div className={classes.Bg}>
-					<div className={classes.darkOverlay}>
-						<NavBar />
-						<Container maxWidth="lg">
-							{/* Content */}
-							<Grid container spacing={2}>
-								<Hidden smDown>
-									<Grid item md>
-										Left Col
-									</Grid>
-								</Hidden>
-								<Grid item md>
-									<div className={classes.authPanel}>
-										<AuthenticationPanel />
-									</div>
-								</Grid>
-							</Grid>
-						</Container>
-					</div>
-				</div>
-			</Fragment>
-		);
-	}
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.Bg}>
+                <div className={classes.darkOverlay}>
+                    <NavBar />
+                    <Container maxWidth="lg">
+                        {/* Content */}
+                        <Grid container spacing={2}>
+                            <Grid item md xs={12}>
+                                <Box
+                                    display="flex"
+                                    justifyContent="center"
+                                    alignItems="center"
+                                    style={{ height: "100%" }}
+                                >
+                                    Left Col
+                                </Box>
+                            </Grid>
+
+                            <Grid item md xs={12}>
+                                <Box
+                                    display="flex"
+                                    justifyContent="center"
+                                    alignItems="center"
+                                    style={{ minHeight: "100vh" }}
+                                    m="auto"
+                                >
+                                    <AuthenticationPanel
+                                        className={classes.authPanel}
+                                    />
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(HomePage);
