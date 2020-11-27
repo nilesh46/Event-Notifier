e21ae50c103ea9860ebd6364c7f2e9a8c75658cf
INNOVATIVEGAMER <55017197+INNOVATIVEGAMER@users.noreply.github.com>
2020-11-28 00:35:37 +0530

    Fixed bug for OauthPanel
    Oauth Buttons resizing and responsiveness upgarded.
    Now buttons are responsive for future updates on all screens.

--- src/features/Panels/OAuthPanel.jsx
+++ src/features/Panels/OAuthPanel.jsx
@@ -4,74 +4,105 @@ import blue from "@material-ui/core/colors/blue";
 import grey from "@material-ui/core/colors/grey";
 
 const style = (theme) => ({
-	root: {
-		backgroundColor: "white",
-		borderTop: "1px dotted #333",
-		paddingTop: "2px",
-	},
+    root: {
+        backgroundColor: "white",
+        borderTop: "1px dotted #333",
+        paddingTop: "2px",
+    },
 });
 
 const FacebookButton = withStyles((theme) => ({
-	root: {
-		color: theme.palette.getContrastText(blue[500]),
-		backgroundColor: blue[500],
-		width: "100%",
-		"&:hover": {
-			backgroundColor: blue[700],
-		},
-	},
+    root: {
+        color: theme.palette.getContrastText(blue[500]),
+        backgroundColor: blue[500],
+        "&:hover": {
+            backgroundColor: blue[700],
+        },
+        flex: 1,
+    },
 }))(Button);
 
 const GoogleButton = withStyles((theme) => ({
-	root: {
-		color: theme.palette.getContrastText(grey[100]),
-		backgroundColor: grey[50],
-		width: "100%",
-		"&:hover": {
-			backgroundColor: grey[300],
-		},
-	},
+    root: {
+        color: theme.palette.getContrastText(grey[100]),
+        backgroundColor: grey[50],
+        "&:hover": {
+            backgroundColor: grey[300],
+        },
+        flex: 1,
+    },
 }))(Button);
 
 const GithubButton = withStyles((theme) => ({
-	root: {
-		color: theme.palette.getContrastText(grey[300]),
-		backgroundColor: grey[350],
-		width: "100%",
-		"&:hover": {
-			backgroundColor: grey[500],
-		},
-	},
+    root: {
+        color: theme.palette.getContrastText(grey[300]),
+        backgroundColor: grey[350],
+        "&:hover": {
+            backgroundColor: grey[500],
+        },
+        flex: 1,
+    },
 }))(Button);
 
 class OAuthPanel extends Component {
-	render() {
-		const { classes } = this.props;
-		return (
-			<div className={classes.root}>
-				<Box display="flex">
-					<GoogleButton variant="contained">
-						<img src="./assets/icons/google.svg" alt="google" />
-						<Typography>
-							<strong>Google</strong>
-						</Typography>
-					</GoogleButton>
-					<FacebookButton variant="contained">
-						<img src="./assets/icons/facebook.svg" alt="facebook" />
-						<Typography>
-							<strong>Facebook</strong>
-						</Typography>
-					</FacebookButton>
-					<GithubButton variant="contained">
-						<img src="./assets/icons/github.svg" alt="github" />
-						<Typography>
-							<strong>GitHub</strong>
-						</Typography>
-					</GithubButton>
-				</Box>
-			</div>
-		);
-	}
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <Box
+                    display="flex"
+                    flexWrap="wrap"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <GoogleButton variant="contained">
+                        <Box
+                            display="flex"
+                            flexWrap="wrap"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <img src="./assets/icons/google.svg" alt="google" />
+                            <Typography>
+                                <strong>Google</strong>
+                            </Typography>
+                        </Box>
+                    </GoogleButton>
+
+                    <FacebookButton variant="contained">
+                        <Box
+                            display="flex"
+                            flexWrap="wrap"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <img
+                                src="./assets/icons/facebook.svg"
+                                alt="facebook"
+                            />
+                            <Typography>
+                                <strong>Facebook</strong>
+                            </Typography>
+                        </Box>
+                    </FacebookButton>
+
+                    <GithubButton variant="contained">
+                        <Box
+                            display="flex"
+                            flexWrap="wrap"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <img src="./assets/icons/github.svg" alt="github" />
+                            <Typography>
+                                <strong>GitHub</strong>
+                            </Typography>
+                        </Box>
+                    </GithubButton>
+                </Box>
+            </div>
+        );
+    }
 }
 
 export default withStyles(style)(OAuthPanel);
