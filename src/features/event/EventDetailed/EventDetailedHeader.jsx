7c201b668d7a5e926a12a1e2b67003b4b889bc69
INNOVATIVEGAMER <55017197+INNOVATIVEGAMER@users.noreply.github.com>
2020-11-27 23:16:18 +0530

    components of event detailed page
    1.Created all components of event detailed page except the chat system.
    2.All the resposiveness is checked for all screen sizes and orientation

---
+++ src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { deepOrange } from "@material-ui/core/colors";
+import { Box, Button } from "@material-ui/core";
+
+const styles = (theme) => ({
+    root: {
+        marginTop: "1rem",
+        marginBottom: "1rem",
+        backgroundColor: "#F5F5F5",
+    },
+    media: {
+        height: 0,
+        paddingTop: "56.25%", // 16:9
+    },
+    avatar: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+    },
+    btn: {
+        marginLeft: "0.3rem",
+        marginRight: "0.3rem",
+    },
+});
+
+class EventDetailedHeader extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <Card className={classes.root}>
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            alt="Host Name"
+                            src="https://source.unsplash.com/300x500/?person"
+                            className={classes.avatar}
+                        ></Avatar>
+                    }
+                    title="Event Title"
+                    subheader="November 27, 2020"
+                />
+                <CardMedia
+                    className={classes.media}
+                    image="https://source.unsplash.com/1600x900/?gaming"
+                    title="Image Title"
+                />
+                <CardContent>
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                    >
+                        Hosted By <strong>Hostname</strong>
+                    </Typography>
+                    <Box
+                        display="flex"
+                        justifyContent="space-between"
+                        mt="0.5rem"
+                    >
+                        <Box>
+                            <Button
+                                variant="contained"
+                                size="small"
+                                className={classes.btn}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="contained"
+                                size="small"
+                                color="primary"
+                                className={classes.btn}
+                            >
+                                Join
+                            </Button>
+                        </Box>
+                        <Box>
+                            <Button
+                                variant="outlined"
+                                size="small"
+                                color="secondary"
+                                className={classes.btn}
+                            >
+                                Manage
+                            </Button>
+                        </Box>
+                    </Box>
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default withStyles(styles)(EventDetailedHeader);
---
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,119 @@
+import {
+    Avatar,
+    Chip,
+    Collapse,
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    withStyles,
+} from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+import GroupIcon from "@material-ui/icons/Group";
+import React, { Component } from "react";
+import indigo from "@material-ui/core/colors/indigo";
+
+const style = (theme) => ({
+    root: {
+        marginTop: "1rem",
+        marginBottom: "1rem",
+    },
+    attendee: {
+        color: theme.palette.getContrastText(indigo[500]),
+        backgroundColor: indigo[500],
+        width: "100%",
+        "&:hover": {
+            backgroundColor: indigo[600],
+        },
+    },
+});
+
+class EventDetailedSidebar extends Component {
+    state = { open: false };
+
+    handleClick = () => {
+        this.setState({ open: !this.state.open });
+    };
+    render() {
+        const { classes } = this.props;
+        return (
+            <List className={classes.root}>
+                <ListItem
+                    button
+                    onClick={this.handleClick}
+                    className={classes.attendee}
+                >
+                    <ListItemIcon>
+                        <GroupIcon style={{ color: "#FFF" }} />
+                    </ListItemIcon>
+                    <ListItemText>
+                        <Typography variant="body1">
+                            <strong>3</strong> People are Going
+                        </Typography>
+                    </ListItemText>
+                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItem button className={classes.nested}>
+                            <ListItemIcon>
+                                <Avatar
+                                    alt="UserName"
+                                    src="https://source.unsplash.com/300x500/?person"
+                                />
+                            </ListItemIcon>
+                            <ListItemText>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    <strong>UserName</strong>
+                                </Typography>
+                            </ListItemText>
+                            <Chip label="HOST" color="secondary" />
+                        </ListItem>
+
+                        <Divider />
+
+                        <ListItem button className={classes.nested}>
+                            <ListItemIcon>
+                                <Avatar
+                                    alt="UserName"
+                                    src="https://source.unsplash.com/300x500/?person"
+                                />
+                            </ListItemIcon>
+                            <ListItemText>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    <strong>UserName</strong>
+                                </Typography>
+                            </ListItemText>
+                        </ListItem>
+                        <ListItem button className={classes.nested}>
+                            <ListItemIcon>
+                                <Avatar
+                                    alt="UserName"
+                                    src="https://source.unsplash.com/300x500/?person"
+                                />
+                            </ListItemIcon>
+                            <ListItemText>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    <strong>UserName</strong>
+                                </Typography>
+                            </ListItemText>
+                        </ListItem>
+                    </List>
+                </Collapse>
+            </List>
+        );
+    }
+}
+
+export default withStyles(style)(EventDetailedSidebar);
