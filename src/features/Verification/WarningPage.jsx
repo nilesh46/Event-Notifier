5c38b5ceeb9e25f477667e18a1379f8a1c51d36a
Nilesh Gupta <guptanilesh2312@gmail.com>
2020-12-20 00:04:20 +0530

    Added Loading Component

--- src/features/Verification/WarningPage.jsx
+++ src/features/Verification/WarningPage.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
+import "../../index.css";
 
 class WarningPage extends Component {
 	render() {
-		return (
-			<div>
-				Dont be OverSmart <br />
-				You cant explore our app without authentication
-			</div>
-		);
+		return <div className="loader">Loading...</div>;
 	}
 }
 
